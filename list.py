639d3afda5e8a02a598110d0bfa9b69659a8923d
Andy Sayler <andy.sayler@gmail.com>
2015-07-07 14:33:12 -0400

    Added relay query

--- list.py
+++ list.py
@@ -3,11 +3,19 @@
 import sys
 
 import click
-import onion_py
+import onion_py.manager
 
 @click.command()
 def list():
     """List University-Affiliated Tor Nodes"""
+
+    manager = onion_py.manager.Manager()
+
+    click.echo("Querying OnionOO for list of relays...")
+    details = manager.query('details')
+
+    relays = details.relays
+    click.echo("Analyzing {:d} relays...".format(len(relays)))
 
     return 0
 
