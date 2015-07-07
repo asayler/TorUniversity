5297c8f8c4e552574cd7beeb67f307f2b5d5a1c0
Andy Sayler <andy.sayler@gmail.com>
2015-07-07 15:25:54 -0400

    Added post processing and better display

--- list.py
+++ list.py
@@ -12,7 +12,7 @@ TLDS = ['edu']
 
 def analyze_relay(relay):
 
-    # Check TLD
+    # Check Hostname TLD
     if relay.host_name:
         clean_hostname = relay.host_name.casefold().strip()
         split_hostname = clean_hostname.split('.')
@@ -35,10 +35,57 @@ def list():
     # Check relays for matches
     relays = details.relays
     click.echo("Analyzing {:d} relays...".format(len(relays)))
+    matched = []
     for relay in relays:
         match = analyze_relay(relay)
         if match:
-            click.echo("{:s}".format(relay.host_name.casefold()))
+            matched.append(relay)
+
+    # Post process
+    exit_relays = []
+    middle_relays = []
+    guard_relays = []
+    other_relays = []
+    for relay in matched:
+        placed = False
+        if relay.exit_probability and (relay.exit_probability > 0):
+            exit_relays.append(relay)
+            placed = True
+        if relay.middle_probability and (relay.middle_probability > 0):
+            middle_relays.append(relay)
+            placed = True
+        if relay.guard_probability and (relay.guard_probability > 0):
+            guard_relays.append(relay)
+            placed = True
+        if not placed:
+            other_relays.append(relay)
+
+    # Display Relays
+    click.echo("")
+    exit_relays.sort(key=lambda x: x.host_name.casefold())
+    middle_relays.sort(key=lambda x: x.host_name.casefold())
+    guard_relays.sort(key=lambda x: x.host_name.casefold())
+    other_relays.sort(key=lambda x: x.host_name.casefold())
+
+    click.echo("Exit Relays: ({:d} found)".format(len(exit_relays)))
+    for relay in exit_relays:
+        click.echo("{:s}".format(relay.host_name.casefold()))
+    click.echo("")
+
+    click.echo("Middle Relays: ({:d} found)".format(len(middle_relays)))
+    for relay in middle_relays:
+        click.echo("{:s}".format(relay.host_name.casefold()))
+    click.echo("")
+
+    click.echo("Guard Relays: ({:d} found)".format(len(guard_relays)))
+    for relay in guard_relays:
+        click.echo("{:s}".format(relay.host_name.casefold()))
+    click.echo("")
+
+    click.echo("Other Relays: ({:d} found)".format(len(other_relays)))
+    for relay in other_relays:
+        click.echo("{:s}".format(relay.host_name.casefold()))
+    click.echo("")
 
     # Return exit status
     return 0
